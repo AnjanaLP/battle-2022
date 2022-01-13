4acfc3e840e6dc376ae5b7a90d5b823a9a92e8e0
AnjanaLP <anjana.lpn@gmail.com>
2022-01-13 12:49:53 +0000

    player 1 can see player 2's hit points

--- spec/features/enter_player_names_spec.rb
+++ spec/features/enter_player_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter player names' do
   scenario 'fill in player names and see them' do
-    visit '/'
-    fill_in :player_1_name, with: "Alice"
-    fill_in :player_2_name, with: "Bob"
-    click_button "Submit"
+    sign_in_and_play
 
     expect(page).to have_content "Alice Vs Bob"
   end
